a18726ded5237dee17cdaecd3948e9444211c6dd
Thejesh Kumar MD <mdtk30.mdtk@gmail.com>
2022-05-04 18:38:04 +0530

    Variables

--- 4.Execution.sh
+++ 4.Execution.sh
@@ -3,3 +3,8 @@ read -p "Enter Your Name :" name
 read -p "Enter Course Name:" course
 
 echo "Your Name=${name},Course Name=${course} "
+echo script name = $0
+echo first argument = $1
+echo second argument = $2
+echo all arguments = $*
+echo no of arguments = $#
